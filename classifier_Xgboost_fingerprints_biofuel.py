db054ad69e4b0aa99475eb4eab45559290e38120
Xiqi <982134384@qq.com>
2024-05-29 21:58:48 +0100

    import pandas as pd
    # Read the Excel file
    excel_file = 'new dataset.xlsx'  # Replace with your Excel file name
    df = pd.read_excel(excel_file)
    
    # Save the DataFrame as a CSV file
    csv_file = 'new dataset.csv'
    df.to_csv(csv_file, index=False)
    
    print(f'The Excel file has been successfully converted to {csv_file}')

--- classifier_Xgboost_fingerprints_biofuel.py
+++ classifier_Xgboost_fingerprints_biofuel.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
-# 读取 Excel 文件
-excel_file = 'new dataset.xlsx'  # 替换为你的 Excel 文件名
+# Read the Excel file
+excel_file = 'new dataset.xlsx'  # Replace with your Excel file name
 df = pd.read_excel(excel_file)
 
-# 将 DataFrame 保存为 CSV 文件
-csv_file = 'dataset.csv'
+# Save the DataFrame as a CSV file
+csv_file = 'new dataset.csv'
 df.to_csv(csv_file, index=False)
 
-print(f'Excel 文件已成功转换为 {csv_file}')
+print(f'The Excel file has been successfully converted to {csv_file}')
+
 
 
 
